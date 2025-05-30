09a3032cfcf781227f12aa365f41cef30de6021e
CipherCrafter <muhamedshijask@kgpian.iitkgp.ac.in>
2025-05-30 16:29:20 +0530

    Prediction Pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -48,8 +48,66 @@ class ModelTrainer:
                 "CatBoostRegressor": CatBoostRegressor(verbose=0)
             }
 
+            params = {
+                "LinearRegression": {},  # no hyperparameters usually tuned here
+
+                "DecisionTreeRegressor": {
+                    "max_depth": [None, 5, 10, 15, 20],
+                    "min_samples_split": [2, 5, 10],
+                    "min_samples_leaf": [1, 2, 4],
+                    "max_features": [None, "sqrt", "log2"]
+                },
+
+                "RandomForestRegressor": {
+                    "n_estimators": [50, 100, 200],
+                    "max_depth": [None, 10, 20, 30],
+                    "min_samples_split": [2, 5],
+                    "min_samples_leaf": [1, 2],
+                    "max_features": ["sqrt", "log2", None],
+                    "bootstrap": [True, False]
+                },
+
+                "GradientBoostingRegressor": {
+                    "n_estimators": [100, 200],
+                    "learning_rate": [0.01, 0.05, 0.1],
+                    "max_depth": [3, 5, 10],
+                    "min_samples_split": [2, 5],
+                    "min_samples_leaf": [1, 2],
+                    "subsample": [0.6, 0.8, 1.0],
+                    "max_features": ["sqrt", "log2", None]
+                },
+
+                # "XGBRegressor": {
+                #     "n_estimators": [100, 200],
+                #     "learning_rate": [0.01, 0.05, 0.1],
+                #     "max_depth": [3, 5, 7],
+                #     "subsample": [0.6, 0.8, 1.0],
+                #     "colsample_bytree": [0.6, 0.8, 1.0]
+                # },
+
+                "KNeighborsRegressor": {
+                    "n_neighbors": [3, 5, 7, 9],
+                    "weights": ["uniform", "distance"],
+                    "p": [1, 2]  # 1 = Manhattan, 2 = Euclidean distance
+                },
+
+                "AdaBoostRegressor": {
+                    "n_estimators": [50, 100, 200],
+                    "learning_rate": [0.01, 0.05, 0.1],
+                    "loss": ["linear", "square", "exponential"]
+                },
+
+                "CatBoostRegressor": {
+                    "iterations": [100, 200, 300],
+                    "learning_rate": [0.01, 0.05, 0.1],
+                    "depth": [4, 6, 8, 10],
+                    "l2_leaf_reg": [1, 3, 5, 7],
+                    "border_count": [32, 50, 100]
+                }
+            }
+
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                              models=models)
+                                              models=models,params=params)
             
             # Sort the model report based on the score
             best_model_score = max(sorted(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import dill
 from src.exception import CustomException
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -17,20 +18,37 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models,params=None):
     try:
         report = {}
 
         for i in range(len(list(models))):
+            model_name = list(models.keys())[i]
             model = list(models.values())[i]
-            model.fit(X_train, y_train)
-            y_train_pred = model.predict(X_train)
-            y_test_pred = model.predict(X_test)
+            param_grid = params.get(model_name, {})
+
+            if param_grid:  # only if params dict not empty
+                gs = GridSearchCV(model, param_grid, cv=3)
+                gs.fit(X_train, y_train)
+                best_model = gs.best_estimator_
+            else:
+                model.fit(X_train, y_train)
+                best_model = model
+
+            y_train_pred = best_model.predict(X_train)
+            y_test_pred = best_model.predict(X_test)
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
             report[list(models.keys())[i]] = test_model_score
         
         return report
     
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
         raise CustomException(e, sys)
